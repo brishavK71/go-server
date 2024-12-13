307f9b7eedfaf0558dadb1fd8dcec29f1ede5561
brishavk71 <brishavk71@gmail.com>
2024-12-13 15:32:06 +0545

    go-server setuped

--- main.go
+++ main.go
@@ -1,13 +1,44 @@
-package main{
+package main
+
+import (
 	"fmt"
 	"log"
 	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "error in ParseForm : %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST was Successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s \n", name)
+	fmt.Fprintf(w, "Address = %s", address)
+
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "405 Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 }
 
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
+	fmt.Printf("Server starting at 8080\n")
 
-
-func main 
-
-
-
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
